d89c175043c588f15ada2bd43e348c7c2bdfaea9
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2019-01-31 23:14:31 +0000

    Add files via upload

---
+++ Aula03 - Condições e instruções de seleção-20190131/tp1.py
@@ -0,0 +1,14 @@
+
+idade = int(input("Idade?")
+
+print("Idade:", idade)
+
+if idade < 18 :
+    print("novo")
+else:
+    if 18 <= idade < 45:
+        print("adulto")
+    else:
+        print("idoso")
+
+print("END")
